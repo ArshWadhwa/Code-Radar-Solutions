3d732fc5d99a690bf25897a35dc6755c38cb782a
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-01-18 18:40:57 +0530

    Solution to Check Divisibility

--- Check Divisibility@@6788dd63490637cfc44e1b91/code.c
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -4,5 +4,5 @@
 int main() {
     int year;
    scanf("%d",&year);
-   year%5==0 && year%11==0 ? printf("Leap Year") : printf("Not a Leap Year");
+   year%5==0 && year%11==0 ? printf("Divisible") : printf("Not Divisible");
 }
