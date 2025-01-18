4e2c1d9b46bddcaf90480fdba889925979c6ee92
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-01-18 18:13:31 +0530

    Solution to Check Equal To

--- Check Equal To@@67882a731b4392d8fc1577da/code.c
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -6,6 +6,6 @@ int main() {
       int a;
     int b;
     scanf("%d %d", &a,&b);
-    a==b ? printf("True") : printf("False");
+    a==b ? printf("True") : printf("False");// if here used single equal to then it will take a=b always
     
 }
