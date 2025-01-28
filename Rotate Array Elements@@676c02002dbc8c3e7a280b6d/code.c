631282d053a7f3d275d9358d557f573cb04cc948
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-01-28 19:21:19 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,53 +1,58 @@
 #include <stdio.h>
 
-
 int main() {
-   int s;
-   scanf("%d",&s);
+    int s;
+    scanf("%d", &s);
 
-    int k;
-    scanf("%d",&k);
+  
 
-    k=k%s;
-   int arr[s];
+    k = k % s; // Normalize k
+    int arr[s];
 
-   for( int i=0;i<s;i++){
-    scanf("%d",&arr[i]);
-   }
-   int start=0;
-   int end=s-1;
-//reverse purra array
-   while(start<=end){
-    int temp=arr[end];
-    arr[end]=arr[start];
-    arr[start]=temp;
-    start++;
-    end--;
+    // Input array elements
+    for (int i = 0; i < s; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+      int k;
+    scanf("%d", &k);
 
-   }
-// fer reverse till k
-int end=k-1;
-int start=0;
-   while(start<end){
-    int temp=arr[end];
-    arr[end]=arr[start];
-    arr[start]=temp;
-    start++;
-    end--;
-   }
-   int start=k;
-   int end=s-1;
-   while(start<=end){
-    int temp=arr[end];
-    arr[end]=arr[start];
-    arr[start]=temp;
-    start++ ;
-    end--;
-   }
+    // Reverse the whole array
+    int start = 0, end = s - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
 
-for(int i=0;i<s;i++){
-    printf("%d\n",arr[i]);
-} 
+    // Reverse the first k elements
+    start = 0;
+    end = k - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
 
+    // Reverse the remaining elements
+    start = k;
+    end = s - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
 
+    // Print the rotated array
+    for (int i = 0; i < s; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
 }
