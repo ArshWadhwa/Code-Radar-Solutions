5167ea1c86be2f2f06764fa4c470610712f4a260
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-02-23 12:48:10 +0530

    Solution to Find the Sum of Digits of Each Element in an Array

--- Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
+++ Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
-int minElement(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
+int main(){
+ for (int i = 0; i < size; i++) {
         int sm = 0;
         if (arr[i] <= 9 && arr[i] > 0) {
             sm = arr[i];
         } else {
             int num = arr[i];
             while (num > 0) {
-                int rem = num % 10;
-                sm += rem;
+                sm += num % 10;
                 num /= 10;
             }
         }
         arr[i] = sm;
     }
 
-
-    int mn = arr[0];
-    for (int i = 1; i < size; i++) {
-        if (arr[i] < mn) {
-            mn = arr[i];
-        }
+    // Printing the transformed array
+    printf("Transformed array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
     }
-    return mn;
+    printf("\n");
+
+    return 0;
+
 }
