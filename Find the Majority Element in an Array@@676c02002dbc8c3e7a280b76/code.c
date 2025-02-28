07511ba18c474bea7dce5179a9dca9fa3f2147e9
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-02-28 20:12:01 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
-  bubbSort(arr,n);
+
+void bubbSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i+1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
 int main(){
 
     int n;
@@ -36,17 +47,6 @@ int main(){
     }
 
 
-}
 
-void bubbSort(int arr[], int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i+1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
 
 }
