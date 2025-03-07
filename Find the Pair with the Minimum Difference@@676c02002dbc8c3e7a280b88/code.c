de495860b6d2297fea973b3bb1e2d8cbb5a11662
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-07 18:05:55 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -20,6 +20,12 @@ int main() {
         }
     }
    }
-   printf("%d %d",num1,num2);
+   if(num1>num2){
+        printf("%d %d",num2,num1);
+   }
+   else{
+    printf("%d %d",num1,num2);
+   }
+   
    return 0;
 }
