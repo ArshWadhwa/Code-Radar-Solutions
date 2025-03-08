897754850a73e70b0c6254ac056737f6a01d2c3f
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-08 14:39:35 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,9 +1,9 @@
-int fibo(int n){
+int fibonacciSeries(int n){
     if(n==0 || n==1){
         return 0;
     }
     if(n>2){
-        return fibo(n-1)+fibo(n-2);
+        return fibonacciSeries(n-1)+fibonacciSeries(n-2);
     }
 
 }
