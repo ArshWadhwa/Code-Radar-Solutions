54748f147b2353e3e094c5b789308ba1ad11b90f
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-08 14:47:32 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,28 +1,27 @@
 void fibonacciSeries(int n){
     int a=0,b=1,nt;
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<n;i++){
         nt=a+b;
         a=b;
         b=nt;
     }
 
-
 }
 
-int main() {
-    int n;
+// int main() {
+//     int n;
 
 
-    scanf("%d", &n);
+//     scanf("%d", &n);
 
 
-    if (n <= 0) {
-        printf("Invalid input\n");
-        return 0;
-    }
+//     if (n <= 0) {
+//         printf("Invalid input\n");
+//         return 0;
+//     }
 
    
-    fibonacciSeries(n);
+//     fibonacciSeries(n);
 
-    return 0;
-}
+//     return 0;
+// }
