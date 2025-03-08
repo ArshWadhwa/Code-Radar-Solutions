7dc46b41792c5a320a5b1820b4f2ee4033c1202f
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-08 14:45:34 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,10 +1,10 @@
 void fibonacciSeries(int n){
-    int a=0,b=1,n;
+    int a=0,b=1,nt;
     for(int i=0;i<=n;i++){
-        n=a+b;
-        b=n;
+        nt=a+b;
+        b=nt;
         a=b;
     }
-    
+
 
 }
