a70b5118c86005c225d1c9cff2f0cd16bf2e5964
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-13 16:29:25 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -20,7 +20,7 @@ printf("%d",l);
 
 
 void bubbSort(int arr[], int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i+1;j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
