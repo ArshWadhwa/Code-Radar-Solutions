22ae74a6deb233b9bf606eb8e963fdef76ca384e
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-16 12:37:27 +0530

    Solution to Search in a Rotated Sorted Array

--- Search in a Rotated Sorted Array@@67ca7acd1fe08d7207c253c4/code.c
+++ Search in a Rotated Sorted Array@@67ca7acd1fe08d7207c253c4/code.c
@@ -2,10 +2,8 @@ int searchInRotatedArray(int arr[],int n,int target){
     int res=-1;
     for(int i=0;i<n;i++){
         if(arr[i]==target){
-            return i;
-        }else {
-            return res;
+            res=i;
         }
     }
-
+return res;
 }
