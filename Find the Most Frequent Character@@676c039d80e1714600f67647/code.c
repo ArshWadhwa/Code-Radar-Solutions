ad2a89d0e14033c134e2bab33bc3f56036d56bb1
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-21 19:24:08 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -20,8 +20,6 @@ for(int i=0;c1[i]!='\0';i++){
             if(cnt>mxfreq && c1[i]<mxChar){
                 mxfreq=cnt;
                 mxChar=c1[i];
-            }else{
-                mxChar=c1[]
             }
 
         }
