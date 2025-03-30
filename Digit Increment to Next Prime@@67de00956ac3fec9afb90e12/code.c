292c972c4a32480d8e02312bac400051545fb57a
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-30 22:16:25 +0530

    Solution to Digit Increment to Next Prime

---
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -0,0 +1,19 @@
+int incrementToPrimeDigits(int N){
+   int temp=N
+    while(temp!=0){
+        temp/=10;
+        cnt++;
+        
+    }
+    int arr[cnt];
+    int l=cnt-1;
+     
+     while(N>0){
+        arr[l]=N%10;
+        N/=10;
+        l--;
+     }
+     for(int i=0;i<cnt;i++){
+        printf("%d",arr[i]);
+     }
+}
