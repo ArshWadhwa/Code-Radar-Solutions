d716fb9d3ea84f8766db818a6196f2c33225fdf6
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-30 22:30:20 +0530

    Solution to Digit Increment to Next Prime

--- Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -1,21 +1,53 @@
-int incrementToPrimeDigits(int N){
-   int temp=N;
-   int cnt=0;
-    while(temp!=0){
-        temp/=10;
-        cnt++;
-        
+#include <stdio.h>
+
+
+int isPrime(int num) {
+    if (num < 2) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
     }
-    int nums=cnt;
-    int arr[cnt];
-    int l=cnt-1;
-     
-     while(N>0){
-        arr[l]=N%10;
-        N/=10;
+    return 1;
+}
+
+// Function to find the next prime number greater than or equal to `digit`
+int nextPrime(int digit) {
+    while (!isPrime(digit)) {
+        digit++;  
+    }
+    return digit;
+}
+
+
+int incrementToPrimeDigits(int N) {
+    int temp = N, cnt = 0;
+
+
+    while (temp != 0) {
+        temp /= 10;
+        cnt++;
+    }
+
+    int arr[cnt]; 
+    int l = cnt - 1;
+
+
+    while (N > 0) {
+        arr[l] = N % 10;
+        N /= 10;
         l--;
-     }
-     for(int i=0;i<nums-1;i++){
-        printf("%d",arr[i]);
-     }
+    }
+
+
+    for (int i = 0; i < cnt; i++) {
+        arr[i] = nextPrime(arr[i]);
+    }
+
+    // Convert modified array back to an integer
+    int newNumber = 0;
+    for (int i = 0; i < cnt; i++) {
+        newNumber = newNumber * 10 + arr[i];
+    }
+
+    return newNumber;
 }
