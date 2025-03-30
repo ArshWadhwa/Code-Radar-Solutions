9c593fb9a92e787ab734c770ea5cc30a6fbd4594
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-30 09:36:20 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,18 @@
+int fact(n){
+    if(n==0){
+        return 1;
+    }
+
+    for(int i=1;i<=n;i++){
+        return n*fact(n-1);
+    }
+
+}
+int factorialRange(int start , int end){
+    if(start>end || start <0 || end<0){
+        return 0;
+    }
+    for(int i=start;i<=end;i++){
+        printf("%d\n",fact(i));
+    }
+}
