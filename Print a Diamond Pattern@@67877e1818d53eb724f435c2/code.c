25a97da79b6a33cc96d28cd36b258d0d8ed5a2f6
Arsh Wadhwa <93117386+ArshWadhwa@users.noreply.github.com>
2025-03-30 13:13:15 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,23 +1,48 @@
+
 #include <stdio.h>
+
 int main() {
     int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
-            printf(" ");
-        }
-        for(int k=0;k<2*i-1;k++){
-            printf("*");
-        }
-        printf("\n");
+    scanf("%d", &n);
+
+    // Top single star
+    printf("*\n");
+
+    // Middle stars: '***' printed (2n - 2) times
+    for (int i = 0; i < 2 * n - 2; i++) {
+        printf("***\n");
     }
-    for(int i=n;i>=0;i--){
-        for(int j=0;j<n-i;j++){
-            printf(" ");
-        }
-        for(int k=0;k<2*i-1;k++){
-            printf("*");
-        }
-        printf("\n");
+
+    // Bottom single star (only if n > 1)
+    if (n > 1) {
+        printf("*\n");
     }
+
+    return 0;
 }
+
+
+
+// #include <stdio.h>
+// int main() {
+//     int n;
+//     scanf("%d",&n);
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-i;j++){
+//             printf(" ");
+//         }
+//         for(int k=0;k<2*i-1;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     for(int i=n;i>=0;i--){
+//         for(int j=0;j<n-i;j++){
+//             printf(" ");
+//         }
+//         for(int k=0;k<2*i-1;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
